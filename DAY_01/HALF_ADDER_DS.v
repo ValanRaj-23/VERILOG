907608dd5983fa950d77e073e8671faeb2101d2c
VALAN RAJ S <valanrajs.2364@gmail.com>
2025-05-14 23:15:54 +0530

    Changes

--- DAY_01/HALF_ADDER_DS.v
+++
@@ -1,7 +0,0 @@
-module half_adder_ds(input A, B,
-			output sum, cr);
-
-	assign sum = A ^ B;
-	assign cr = A & B;
-
-endmodule
--- DAY_02/Encoder/encoder_8x3_ds.v
+++
@@ -1,8 +0,0 @@
-module encoder_8x3_ds(input [7:0]d,
-                      output reg [2:0]y);
-  
-  assign y[0] = d[1] + d[3] + d[5] + d[7];
-  assign y[1] = d[2] + d[3] + d[6] + d[7];
-  assign y[2] = d[4] + d[5] + d[6] + d[7];
- 
-endmodule      
